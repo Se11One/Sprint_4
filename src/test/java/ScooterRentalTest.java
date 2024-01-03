3f92f189ed28eb0ec57742ecc64de9cf752c36b0
Atyom Selyutin <rollaman4212@gmail.com>
2024-01-03 21:49:30 +1000

    исправление наименований тестов (названия начинаются с маленькой буквы)

--- src/test/java/ScooterRentalTest.java
+++ src/test/java/ScooterRentalTest.java
@@ -57,7 +57,7 @@ public class ScooterRentalTest {
     }
 
     @Test
-    public void OrderScooterForTopButton() {
+    public void orderScooterForTopButton() {
         driver.get("https://qa-scooter.praktikum-services.ru/");
         MainPage mainPage = new MainPage(driver);
         mainPage.acceptCookies();
@@ -77,7 +77,7 @@ public class ScooterRentalTest {
     }
 
     @Test
-    public void OrderScooterForBottomButton() {
+    public void orderScooterForBottomButton() {
         driver.get("https://qa-scooter.praktikum-services.ru/");
         MainPage mainPage = new MainPage(driver);
         mainPage.acceptCookies();
